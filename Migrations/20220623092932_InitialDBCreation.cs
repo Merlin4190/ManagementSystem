e01f5c5bd86bf720a0b21eed7d1e5d03d243e81b
Ayodeji Bolanle <ayodeji.bolanle@nlpcpfa.com>
2022-06-23 13:00:29 +0100

    Add project files.

---
+++ Migrations/20220623092932_InitialDBCreation.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ManagementSystem.Migrations
+{
+    public partial class InitialDBCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    PhotoUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhotoPublicId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Visitors",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    VisitingDepartment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Purpose = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Feedback = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TimeIn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TimeOut = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    WalkInType = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Visitors", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Visitors");
+        }
+    }
+}
---
+++ Migrations/20220623101709_InitialSeedData.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ManagementSystem.Migrations
+{
+    public partial class InitialSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhotoUrl",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhotoPublicId",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "DateCreated", "DateUpdated", "Email", "EmailConfirmed", "FirstName", "Gender", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "PhotoPublicId", "PhotoUrl", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "c9d4c053-49b6-410c-bc78-2d54a9991870", 0, "507b18a5-9cce-4608-a9a3-4e2aa4075894", new DateTime(2022, 6, 23, 11, 17, 9, 405, DateTimeKind.Local).AddTicks(8660), new DateTime(2022, 6, 23, 11, 17, 9, 405, DateTimeKind.Local).AddTicks(8677), "admin@nlpcpfa.com", true, "IT", "Male", "Support", false, null, null, null, null, null, false, null, null, "d472fe17-1aa9-4bab-8124-1a627ac9570a", false, "admin" });
+
+            migrationBuilder.InsertData(
+                table: "Visitors",
+                columns: new[] { "Id", "CreatedAt", "Feedback", "FirstName", "Gender", "LastName", "Purpose", "TimeIn", "TimeOut", "UpdatedAt", "VisitingDepartment", "WalkInType" },
+                values: new object[] { "80abbca8-664d-4b20-b5de-024705497d4a", new DateTime(2022, 6, 23, 11, 17, 9, 405, DateTimeKind.Local).AddTicks(9169), "Experience was wonderful", "Arsene", 0, "Merlino", "Test purpose", new DateTime(2022, 6, 23, 11, 17, 9, 405, DateTimeKind.Local).AddTicks(9179), new DateTime(2022, 6, 23, 13, 17, 9, 405, DateTimeKind.Local).AddTicks(9180), new DateTime(2022, 6, 23, 11, 17, 9, 405, DateTimeKind.Local).AddTicks(9169), "IT Department", 1 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "c9d4c053-49b6-410c-bc78-2d54a9991870");
+
+            migrationBuilder.DeleteData(
+                table: "Visitors",
+                keyColumn: "Id",
+                keyValue: "80abbca8-664d-4b20-b5de-024705497d4a");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhotoUrl",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhotoPublicId",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
